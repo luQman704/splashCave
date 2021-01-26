43f6384f2a68e20b40b43e446ec9dc514c1696c6
luQman704 <olukoya42@hotmail.com>
2021-01-26 13:00:40 -0800

    ghga

---
+++ storage/framework/views/96fdca85073c8ac763a752a9890a1aaa48990ee7.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <script src="https://use.fontawesome.com/f896ad6829.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Analytics Dashboard - This is an example dashboard created using build-in elements and components.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+    <meta name="msapplication-tap-highlight" content="no">
+    <!--
+    =========================================================
+    * ArchitectUI HTML Theme Dashboard - v1.0.0
+    =========================================================
+    * Product Page: https://dashboardpack.com
+    * Copyright 2019 DashboardPack (https://dashboardpack.com)
+    * Licensed under MIT (https://github.com/DashboardPack/architectui-html-theme-free/blob/master/LICENSE)
+    =========================================================
+    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    -->
+<link href="../css/main.css" rel="stylesheet"></head>
+<body>
+    <div  class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+        <div id="app" class="app-main">
+                    <app></app>
+                <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+    </div>
+<script type="text/javascript" src="../js/app.js"></script>
+</body>
+
+</html>
+<?php /**PATH C:\Users\SURFACE\Desktop\splashCave\resources\views/welcome.blade.php ENDPATH**/ ?>
